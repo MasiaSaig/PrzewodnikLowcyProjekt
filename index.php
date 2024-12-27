8fc7c8a53c3788bdc91d4b0b36238a68e7ab7c3e
MasiaSaig <mullermaciej12@gmail.com>
2024-12-27 17:15:31 +0100

    idk...

--- index.php
+++ index.php
@@ -3,6 +3,18 @@ session_start();
 
 require "database.php";
 $loggedIn = false;
+function loggedIn(){
+	// check cookie if user is logged in
+	global $pdo;
+	$loggedInCheck = $pdo->prepare("SELECT (token_autoryzacji = :authLoginCookie) FROM prj.łowca WHERE token_autoryzacji=:authLoginCookie");
+	$loggedInCheck->execute(['authLoginCookie' => $_COOKIE['authLoginToken']]);
+	if($loggedInCheck->fetchColumn() == 1){
+		// user already logged in
+		return true;
+	}// else could not redirect, possibly wrong cookie loginToken, or user is just not logged in.
+	return false;
+}
+
 try{
 	$loggedIn = loggedIn();
 }catch(PDOException $e){
